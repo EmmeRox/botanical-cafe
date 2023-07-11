0bae3d309c64391f5ff3844c466d615906efd9f1
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-07-11 12:02:54 -0500

    styling the hero

--- botanical-cafe/src/components/Home.jsx
+++ botanical-cafe/src/components/Home.jsx
@@ -1,7 +1,23 @@
+import "./Home.css";
+
 function Home() {
   return (
-    <div className="background-img">
-      <h1 className="font-extrabold">Hello Botanical Cafe</h1>
+    <div className="background-img md:flex md:space-x-3 p-40 border-4 m-auto max-w-5xl border-pink-300">
+      <div className="justify-center pr-20 md:pt-10">
+        <h1 className="font-extrabold text-center text-3xl md:text-6xl text-white">
+          Botanical Cafe
+        </h1>
+        <h2 className="text-white pt-10 text2xl md:text-xl">
+          Chicago's First Plant Shop and Cafe!
+        </h2>
+      </div>
+      <div className="items-center justify-center">
+        <img
+          src="../../public/temp-bc-logo.jpg"
+          alt="logo"
+          className="rounded-full bc-logo"
+        />
+      </div>
     </div>
   );
 }
