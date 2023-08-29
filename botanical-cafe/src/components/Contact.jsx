6e7476474662dff2a5b580e44bd5b9a77e98480a
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-08-29 17:40:33 -0500

    minor changes

--- botanical-cafe/src/components/Contact.jsx
+++ botanical-cafe/src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       <h1 className="contact font-bold">Contact Us</h1>
       <div className="infopart flex justify-between m-auto max-w-5xl p-10">
         <h3>Phone</h3>
-        <p>lorem</p>
+        <h3>lorem</h3>
         <p>lorem</p>
       </div>
     </>
